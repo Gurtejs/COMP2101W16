be63eb3ff1da555968a7d297938d40af5033f16c
gurtejs <dhaliwal.423@gmail.com>
2016-02-02 21:36:40 +0000

    $ this is my secret file

--- bash/lab1/helloscript.sh
+++ bash/lab1/helloscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #helloscript.sh - this is script display the string "Hello World!"
-#It takes ni input
+#It takes no input
 
 #This is a silly way of creating the output string
 echo -n "helb wold" |sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk '{print $1 "\x20" $2 "\41"}'
